64f704be4420d44a04bd646816a5115635d9689f
gui <gui.wb@hotmail.com>
2026-09-03 19:15:31 -0300

    feat: init

---
+++ src/main/java/br/edu/ifsul/cstsi/tds_guilherme/TdsGuilhermeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.tds_guilherme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdsGuilhermeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdsGuilhermeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifsul/cstsi/tds_guilherme/TdsGuilhermeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.tds_guilherme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdsGuilhermeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
